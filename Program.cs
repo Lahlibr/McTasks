9ea7ac3048980032c7dea20db2e786445a80f7aa
lahlibr <lahlibrahim.72@gmail.com>
2025-05-19 17:34:12 +0530

    secoundcommit

--- Program.cs
+++ Program.cs
@@ -4,12 +4,18 @@
     {
         static void Main(string[] args)
         {
+            
             string input = "3,5";
-            string[] parts = input.Split(",");
-            int num1 = int.Parse(parts[0]);
-            int num2 = int.Parse(parts[1]);
-            int result = num1 * num2;
+            int result = multi(input);
             Console.WriteLine(result);
+            static int multi(string input)
+            {
+                string[] parts = input.Split(",");
+                int num1 = int.Parse(parts[0]);
+                int num2 = int.Parse(parts[1]);
+                return  num1 * num2;
+            }
+            
         }
     }
 }
