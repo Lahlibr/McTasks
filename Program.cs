89f4a025ffd5a4776c1ff4c05347cd70d8611a46
lahlibr <lahlibrahim.72@gmail.com>
2025-05-19 17:07:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "3,5";
+            string[] parts = input.Split(",");
+            int num1 = int.Parse(parts[0]);
+            int num2 = int.Parse(parts[1]);
+            int result = num1 * num2;
+            Console.WriteLine(result);
+        }
+    }
+}
